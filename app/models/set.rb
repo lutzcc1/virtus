46355d1f28198d138a6312b1776d8396342d09f6
Gerardo Lankenau <glankenau@hey.com>
2024-11-19 13:02:27 +0100

    Delete old set model

--- app/models/set.rb
+++
@@ -1,7 +0,0 @@
-class Set < ApplicationRecord
-  belongs_to :routine
-  belongs_to :exercise
-
-  validates :reps_goal, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :total_reps, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-end
